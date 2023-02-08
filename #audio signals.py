03dfa43b5ea89078794d578569dc298ef8736f26
AbaDragon47 <aonumah47@gmail.com>
2023-02-07 20:33:33 -0600

    yuytuyt

---
+++ #audio signals.py
@@ -0,0 +1,6 @@
+#audio signals
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+frequency_sampling, audio_signal= wavfile.read("")#this is where we put the audio file in
