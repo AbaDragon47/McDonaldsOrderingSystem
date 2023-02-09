43867e0819078b716d79459640ed507ca514a24f
AbaDragon47 <aonumah47@gmail.com>
2023-02-09 12:29:47 -0600

    yoooo speech recognition workssss
    and its so fucking cool like i cant even-- literally fan girling like
    the computer knows what I am saying...
    IN DIFFERENT LANGUAGES TOO!

--- import speech_recognition as sr.py
+++ import speech_recognition as sr.py
@@ -1,11 +1,22 @@
 import speech_recognition as sr
 
 recording = sr.Recognizer()
-with sr.Microphone() as source: recording.adjust_for_ambient_noise(source)
-print("Please say something: ")
-audio = recording.listen(source)
+with sr.Microphone() as source: 
+    recording.adjust_for_ambient_noise(source)
+    print("Listening... ")
+    audio = recording.listen(source)
+    global wasSaid
 
-try:
-    print("You said: \n"+ recording.recognize_google(audio))
+#english en-in, en-us
+#mandarin zh
+
+try: 
+    wasSaid = recording.recognize_google(audio, language='en-in')
+    print("You said: \n",wasSaid)
 except Exception as e:
     print(e)
+words = str(wasSaid).split(' ')
+#print("this is the list: ",words)
+
+if(words.__contains__("weather")):
+    print("you need help its alr cold...duh")
