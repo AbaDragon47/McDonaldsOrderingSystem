c120dbabc3555dfbadb739136779ca394c04384b
AbaDragon47 <aonumah47@gmail.com>
2023-02-15 12:26:50 -0600

    importsnt

--- import speech_recognition as sr.py
+++ import speech_recognition as sr.py
@@ -1,22 +1,34 @@
 import speech_recognition as sr
+from numpy import loadtxt
+import tensorflow as tf
+from keras.models import Sequential
+#CALVIN  FUNNNNNNN
+def speakerListener():
+    recording = sr.Recognizer()
+    with sr.Microphone() as source: 
+        recording.adjust_for_ambient_noise(source)
+        print("Listening... ")
+        audio = recording.listen(source)
+        global wasSaid
+    ####################################################################
+    #random stuff DO NOT ignoge
+    #n is 5... but remember 3n is 7                <==      *IMPORTSNT*
+    #english en-in, en-us
+    #mandarin zh
+    #wo de ming zi shi fun wen bing ==> My name is Fun Wen-Bing (Calvin)
+    #####################################################################
+    try: 
+        wasSaid = recording.recognize_google(audio, language='en-in')
+        print("You said: \n",wasSaid)
+    except Exception as e:
+        print(e)
+    words = str(wasSaid).split(' ')
+    #print("this is the list: ",words)
 
-recording = sr.Recognizer()
-with sr.Microphone() as source: 
-    recording.adjust_for_ambient_noise(source)
-    print("Listening... ")
-    audio = recording.listen(source)
-    global wasSaid
+speakerListener()
 
-#english en-in, en-us
-#mandarin zh
 
-try: 
-    wasSaid = recording.recognize_google(audio, language='en-in')
-    print("You said: \n",wasSaid)
-except Exception as e:
-    print(e)
-words = str(wasSaid).split(' ')
-#print("this is the list: ",words)
+
 
 if(words.__contains__("weather")):
     print("you need help its alr cold...duh")
