db9b10eb910029c7639ff7ca9f46a3c8c7adf1d8
AbaDragon47 <aonumah47@gmail.com>
2023-03-01 13:04:22 -0600

    so things have been update again
    its been a while but finally the match() method works and now working on clarifying tags

--- import speech_recognition as sr.py
+++ import speech_recognition as sr.py
@@ -1,7 +1,11 @@
+from gtts import gTTS
+import os
+
 import speech_recognition as sr
 from numpy import loadtxt
 import tensorflow as tf
 from keras.models import Sequential
+from pandas import read_excel
 #CALVIN  FUNNNNNNN
 def speakerListener():
     recording = sr.Recognizer()
@@ -9,27 +13,105 @@ def speakerListener():
         recording.adjust_for_ambient_noise(source)
         print("Listening... ")
         audio = recording.listen(source)
-        global wasSaid
+        global wasSaid, words
     ####################################################################
     #random stuff DO NOT ignoge
     #n is 5... but remember 3n is 7                <==      *IMPORTSNT*
     #english en-in, en-us
     #mandarin zh
+    
     #WWOH DUH MING ZSOU SU FUN WHEN BIENG 
     #wo de ming zi shi fun wen bing ==> My name is Fun Wen-Bing (Calvin)
     #####################################################################
     try: 
-        wasSaid = recording.recognize_google(audio, language='en-in')
-        print("You said: \n",wasSaid)
+        saying = recording.recognize_google(audio, language='en-in')
+        wasSaid= (str)(saying)
+        wasSaid= wasSaid.title()
+        #print("You said: ",wasSaid)
     except Exception as e:
         print(e)
-    words = str(wasSaid).split(' ')
+    #words = str(wasSaid).split(' ')
     #print("this is the list: ",words)
-
+    
 speakerListener()
 
+#Dict is a 'Map' in python
+
+#need to make a method  that take everything in spreadsheet, put it into dict
+# then can take what was said in listening() and figure out what person wants
+
+senStemsWTags= dict()
+qSheet= "questions"
+file_name= 'D:\github stuff\Robacal\menu.xlsx'
+df= read_excel(file_name,sheet_name= qSheet)
+
+#heheeheh... this works lmao
+for i in range(0, len(df)):
+    sen=(str)(df.iloc[i][0])
+    senStemsWTags.update({sen.title():df.iloc[i][1:].values.tolist()})
+# dictionary of stems and tags
+#print(senStemsWTags.items())
+
+
+#method checks to see if sen stems are in what customer says
+tags= []
+def match(words):
+   # print("running")
+    keys=[]
+    count=0
+    for key in senStemsWTags.keys():
+        count+=1
+        actual=key.split(" (")[0]
+        actual=actual.split("With")[0]
+        #print("actual then words ",actual,'\n',words)
+        #print("\n",str(actual) in str(words),"\n\n")
+        #print(str(words).find(actual))
+        
+        #print(words.find(actual))
+        #if "Vanilla Shake" in words:
+         #       print("yes maam",words)
+         #i literally dont know  why the below dont work
+        #print(count)
+        #print("tag: ",actual,"what was said: ",words)
+        if actual in words:
+            tags.extend(senStemsWTags[key])
+            keys.append(key)
+
+    for item in tags:
+        if isinstance(item,float):
+            tags.remove(item)
+    if len(keys)>0:
+        tags.append("Items ==> ")
+    tags.extend(keys)
+
+    if len(tags)==0:
+        return "customer doesnt want to order"
+    else:
+        return tags 
+
+print(match(wasSaid.title()))
+
+
+#clarification method to get specfic tags
+def clarify(list):
+    #re.split(r'#|#',(str))
+    copy=wasSaid.title()
+    newList=[]
+    regList=[]
+    for value in list[list.index("Items ==> ")+1:]:
+        newList.append(value.replace("With"," (").split(" (")[0])
+        regList.extend(value.replace("With"," (").replace(")"," (").split(" (")[1:])
+    for item in newList:
+        if newList.count(item)>1:
+            print("ask for clarification of tags")
+    return regList
+
+print(clarify(tags))
+
+
+#buying method if order
+#def matchTags():
+ #   if(match(wasSaid)=="customer doesnt want to order"):
 
 
 
-if(words.__contains__("weather")):
-    print("you need help its alr cold...duh")
