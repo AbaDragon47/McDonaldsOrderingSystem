f3a9ebf68fc26138dd0607d57c218030a6ab1081
RocioSAguirre <112421542+RocioSAguirre@users.noreply.github.com>
2023-03-06 12:39:15 -0600

    lmao i rocio and i de best
    lololololooololololololololololololol

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-#LMAO we gotta use Kivy now
-#this is Abas file to tinker with
+cart = []
+total = 0
 
-import kivy
-# app refers to the instance of the app
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+# Defining main function
+def main():
+    print("Hello customer!\nWhat would you like to do today?")
+#grab specfic item user wants to order and adds it to cart(array of food items)
+def order(item):
+    cart.append(item)
+    total = total + getPrice(item) #adding up price of order
 
-class KVBL(BoxLayout):
-    #this just creates the boxlayout root widget used in the kv file
-    pass
-class KVBoxLayoutApp(App):
-    def build(self):
-        #returns instance of about class
-        return KVBL
-root= KVBoxLayoutApp()
-root.run
+    print("Your item was added!\nWould you like to order anything else (y/n)?")
+    answ = speakerListener()
+
+    if(answ.contains("y")):
+        print("What would you like to order?")
+        answ = speakerListener() #speakers reponse
+        tags = match(answ) #returns list of key words in response
+        listItems = clarify(tags) #figures out what items it wants to order from key words, returns a list
+        if listItems.len > 0:
+            massOrder(listItems)
+        order(listItems[0])
+    #after finishing ordering, calls methods for paying
+    else:
+        print("You've finished ordering!\nYour total is: " + total)
+        remaining = getCardAmount() - total
+        print("Your bank account balance is: " + remaining)
+        print("Your food will be delivered in 15 min\nP.S ice cream machine still broken..." )
+
+        
+#method when ordering more than 1 item at a time
+def massOrder(listItems):
+    numberOfItems = listItems.len
+    for i in range(numberOfItems):
+        mItem = listItems[i]
+        if i == numberOfItems-1:
+            order(mItem)
+        cart.append(mItem)
+        total = total + getPrice(mItem)  
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
--- mcData.py
+++ mcData.py
@@ -239,23 +239,42 @@ def getPrice(item):
 
 
 cart = []
-cartAdded = False
-orderAgain = False
+total = 0
 #grab specfic item user wants to order and adds it to cart(array of food items)
-#returns boolean if order was placed
-def order(itemArr):
-    cart.append(itemArr)
+def order(item):
+    cart.append(item)
+    total = total + getPrice(item) #adding up price of order
 
     print("Your item was added!\nWould you like to order anything else (y/n)?")
     answ = speakerListener()
+
     if(answ.contains("y")):
-        orderAgain = True
-    if(orderAgain):
-        for itemsOrdering in clarify():
-            order(itemsOrdering)
+        print("What would you like to order?")
+        answ = speakerListener() #speakers reponse
+        tags = match(answ) #returns list of key words in response
+        listItems = clarify(tags) #figures out what items it wants to order from key words, returns a list
+        if listItems.len > 0:
+            massOrder(listItems)
+        order(listItems[0])
+    #after finishing ordering, calls methods for paying
     else:
-        print("You've finished ordering!")
-        return True
+        print("You've finished ordering!\nYour total is: " + total)
+        remaining = getCardAmount() - total
+        print("Your bank account balance is: " + remaining)
+        print("Your food will be delivered in 15 min\nP.S ice cream machine still broken..." )
+
+        
+#method when ordering more than 1 item at a time
+def massOrder(listItems):
+    numberOfItems = listItems.len
+    for i in range(numberOfItems):
+        mItem = listItems[i]
+        if i == numberOfItems-1:
+            order(mItem)
+        cart.append(mItem)
+        total = total + getPrice(mItem)
+        
+
         
 
 
